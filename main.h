0474bd5e2618d77dd693fc1af3f0354f3acccc6d
kamurano <kamrannn.mk@gmail.com>
2024-04-25 19:40:11 +0400

    create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAINH
+#define MAINH
+#define PATH "PATH="
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
+#endif
