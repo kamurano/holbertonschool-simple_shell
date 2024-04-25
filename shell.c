c5f627c065cb947003a18487193fa00919c773ee
kamurano <kamrannn.mk@gmail.com>
2024-04-25 21:16:17 +0400

    test ls added for shell

--- shell.c
+++ shell.c
@@ -2,16 +2,35 @@
 
 #define max_len 100
 
-void handle_command(char *command) {
+void handle_command(char *command)
+{
 	if (strncmp(command, "echo", 4) == 0)
 		printf("%s\n", command + 5);
 	else if (strcmp(command, "exit") == 0)
 		exit(0);
+	else if (strncmp(command, "ls", 2) == 0)
+	{
+		DIR *dir;
+		struct dirent *ent;
+
+		dir = opendir(".");
+if (dir != NULL)
+		{
+			while ((ent = readdir(dir)) != NULL)
+				printf("%s\n", ent->d_name);
+			closedir(dir);
+		}
+		else
+		{
+			perror("");
+		}
+	}
 	else
 		printf("Unknown command: %s\n", command);
 }
 
-int main() {
+int main(void)
+{
 	char command[max_len];
 
 	while (1)
@@ -22,5 +41,5 @@ int main() {
 
 		handle_command(command);
 	}
-	return 0;
+	return (0);
 }
