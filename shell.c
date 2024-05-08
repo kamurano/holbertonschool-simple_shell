b7dd1f8fa3c42eb27109524cee109cb7d8881cf9
samiribrh <samiribrahimov2277@gmail.com>
2024-05-08 23:29:10 +0400

    Memory Leak trial

--- shell.c
+++ shell.c
@@ -41,6 +41,7 @@ void handle_command(char *u_command)
 		if (access(args[0], X_OK) == -1)
 		{
 			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			free(path);
 			free(path_env);
 			exit(127);
 		}
