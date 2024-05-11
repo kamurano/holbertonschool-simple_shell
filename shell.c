0e8e6f66f7a460e1061524c4a55e14fd9e380dcb
kamurano <kamrannn.mk@gmail.com>
2024-05-11 23:27:34 +0400

    fix Betty whitespace

--- shell.c
+++ shell.c
@@ -60,88 +60,88 @@ void execute_command(char **args, char *path)
 }
 void parse_command(char *u_command, char **args)
 {
-    char *command = strtok(u_command, " \t");
-    int i = 0;
+	char *command = strtok(u_command, " \t");
+	int i = 0;
 
-    args[0] = NULL;
-    while (command != NULL && i < MAX_LEN - 1)
-    {
-        args[i] = command;
-        i++;
-        command = strtok(NULL, " \t");
-    }
-    args[i] = NULL;
+	args[0] = NULL;
+	while (command != NULL && i < MAX_LEN - 1)
+	{
+		args[i] = command;
+		i++;
+		command = strtok(NULL, " \t");
+	}
+	args[i] = NULL;
 }
 void handle_path(char **args, char **path, char **path_env, int *found)
 {
-    char *path_token = NULL;
+	char *path_token = NULL;
 
-    if (*path_env == NULL)
-    {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        free(*path_env);
-        free(*path);
-        exit(127);
-    }
-    path_token = strtok(*path_env, ":");
+	if (*path_env == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		free(*path_env);
+		free(*path);
+		exit(127);
+	}
+	path_token = strtok(*path_env, ":");
 
-    while (path_token != NULL)
-    {
-        strcpy(*path, path_token);
-        strcat(*path, "/");
-        strcat(*path, args[0]);
-        if (access(*path, X_OK) != -1)
-        {
-            *found = 1;
-            break;
-        }
-        path_token = strtok(NULL, ":");
-    }
-    free(*path_env);
+	while (path_token != NULL)
+	{
+		strcpy(*path, path_token);
+		strcat(*path, "/");
+		strcat(*path, args[0]);
+		if (access(*path, X_OK) != -1)
+		{
+			*found = 1;
+			break;
+		}
+		path_token = strtok(NULL, ":");
+	}
+	free(*path_env);
 }
 void handle_command(char *u_command)
 {
-    char *args[MAX_LEN], *path = NULL, *path_env = NULL;
-    int found = 0;
+	char *args[MAX_LEN], *path = NULL, *path_env = NULL;
+	int found = 0;
 
-    setup_environment(&path_env, &path);
-    parse_command(u_command, args);
+	setup_environment(&path_env, &path);
+	parse_command(u_command, args);
 
-    if (args[0] == NULL)
-    {
-        free(path_env);
-        free(path);
-        return;
-    }
+	if (args[0] == NULL)
+	{
+		free(path_env);
+		free(path);
+		return;
+	}
 
-    if (strchr(args[0], '/') != NULL)
-    {
-        if (access(args[0], X_OK) == -1)
-        {
-            fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-            free(path);
-            free(path_env);
-            exit(127);
-        }
-        free(path);
-        free(path_env);
-        path = strdup(args[0]);
-        found = 1;
-    }
-    else
-    {
-        handle_path(args, &path, &path_env, &found);
-    }
+	if (strchr(args[0], '/') != NULL)
+	{
+		if (access(args[0], X_OK) == -1)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			free(path);
+			free(path_env);
+			exit(127);
+		}
+		free(path);
+		free(path_env);
+		path = strdup(args[0]);
+		found = 1;
+	}
+	else
+	{
+		handle_path(args, &path, &path_env, &found);
+	}
 
-    if (found == 0)
-    {	
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        free(path);
-        exit(127);
-    }
+	if (found == 0)
+	{	
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		free(path);
+		exit(127);
+	}
 
-    execute_command(args, path);
-    free(path);	
+	execute_command(args, path);
+	free(path);	
 }
 int main(void)
 {
